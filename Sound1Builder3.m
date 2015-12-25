874b203d7579c86cbd632acf23d9e02835d55b98
pialin <765422908@qq.com>
2015-12-25 12:26:33 +0800

    sym+

--- Sound1Builder3.m
+++ Sound1Builder3.m
@@ -2,6 +2,8 @@ clear;
 syms mel y t f;
 fi = sym('f',[3,1]);
 infi = sym('f',[3,1]);
+AmpR = sym('AmpR',[3,1]);
+
 
 f =sym('700*exp(mel/1127-1)');
 
@@ -11,7 +13,9 @@ AudioSampleRate = 48000;
 
 TimeSum = 3;
 
-valuef = zeros(1,round(TimeSum*AudioSampleRate));
+NumSumPoint = round(TimeSum*AudioSampleRate);
+ValueIntf = zeros(1,NumSumPoint);
+ValueAmpR = zeros(1,NumSumPoint);
 
 
 LenSum = 2.4;
@@ -33,12 +37,54 @@ NumSegPoint(iSeg) = round(TimeSeg(iSeg)*AudioSampleRate);
 SegStartPoint(iSeg+1) = SegStartPoint(iSeg) +NumSegPoint(iSeg);
 
 fi(iSeg) = subs(f,y,sym('0.9-0.8*t'));
-
 intfi(iSeg) = int(fi(iSeg),t,0,t);
 
-valuef(SegStartPoint(iSeg):SegStartPoint(iSeg+1)-1) = subs(intfi(iSeg),t,linspace(0,TimeSeg(iSeg),NumSegPoint(iSeg)));
+AmpR(iSeg) = sym('0.1');
+ValueIntf(SegStartPoint(iSeg):SegStartPoint(iSeg+1)-1) = ...
+    double(subs(intfi(iSeg),t,linspace(0,TimeSeg(iSeg),NumSegPoint(iSeg))));
+ValueAmpR(SegStartPoint(iSeg):SegStartPoint(iSeg+1)-1) = ...
+    double(subs(AmpR(iSeg),t,linspace(0,TimeSeg(iSeg),NumSegPoint(iSeg))));
+
 
 iSeg = iSeg +1;
 %¶Î2 _
+LenSeg(iSeg) = LenSum/3;
+TimeSeg(iSeg) =  LenSeg(iSeg)*CoefLen2Time;
+NumSegPoint(iSeg) = round(TimeSeg(iSeg)*AudioSampleRate);
+SegStartPoint(iSeg+1) = SegStartPoint(iSeg) +NumSegPoint(iSeg);
 
+fi(iSeg) = subs(f,y,'0.1');
+intfi(iSeg) = int(fi(iSeg),t,0,t);
+
+AmpR(iSeg) = sym('0.8*t+0.1');
+
+ValueIntf(SegStartPoint(iSeg):SegStartPoint(iSeg+1)-1) = ...
+    double(subs(intfi(iSeg),t,linspace(0,TimeSeg(iSeg),NumSegPoint(iSeg))))+ValueIntf(SegStartPoint(iSeg)-1);
+ValueAmpR(SegStartPoint(iSeg):SegStartPoint(iSeg+1)-1) = ...
+    double(subs(AmpR(iSeg),t,linspace(0,TimeSeg(iSeg),NumSegPoint(iSeg))));
+
+
+iSeg = iSeg +1 ;
 %¶Î3 |
+
+LenSeg(iSeg) = LenSum/3;
+TimeSeg(iSeg) =  LenSeg(iSeg)*CoefLen2Time;
+NumSegPoint(iSeg) = round(TimeSeg(iSeg)*AudioSampleRate);
+
+
+fi(iSeg) = subs(f,y,'0.8*t+0.1');
+intfi(iSeg) = int(fi(iSeg),t,0,t);
+
+AmpR(iSeg) = sym('0.9');
+
+ValueIntf(SegStartPoint(iSeg):end) = ...
+    double(subs(intfi(iSeg),t,linspace(0,TimeSeg(iSeg),NumSegPoint(iSeg))))+ValueIntf(SegStartPoint(iSeg)-1);
+ValueAmpR(SegStartPoint(iSeg):end) = ...
+    double(subs(AmpR(iSeg),t,linspace(0,TimeSeg(iSeg),NumSegPoint(iSeg))));
+
+
+
+ValueAmpL = 1 - ValueAmpR; 
+t = linspace(0,TimeSum,NumSumPoint);
+
+DataAudio= [ValueAmpL;ValueAmpR].*repmat(cos(2*pi*ValueIntf.*t),2,1);
